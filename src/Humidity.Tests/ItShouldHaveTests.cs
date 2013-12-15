6bda3194293a7eb1431d4ba5847a950440e25ffa
Ben Scott <ben.scott@readify.net>
2013-12-15 12:50:51 +1000

    bootstrapped the projects

---
+++ src/Humidity.Tests/ItShouldHaveTests.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Humidity.Tests
+{
+    public class ItShouldHaveTests
+    {
+        [Fact]
+        public void BecauseTestsAreTehBomb()
+        {
+            true.ShouldNotBe(false);
+        }
+    }
+}
---
+++ src/Humidity.Tests/TestHelpers/ConcernFor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humidity.Tests.TestHelpers
+{
+    public abstract class ConcernFor<T>
+    {
+        protected T Subject { get; private set; }
+
+        public ConcernFor()
+        {
+            Context();
+            Subject = Given();
+            When();
+        }
+
+        protected virtual void Context()
+        {
+        }
+
+        protected abstract T Given();
+
+        protected virtual void When()
+        {
+        }
+    }
+}
